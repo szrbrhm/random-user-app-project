0978b9222b7bcd3256b0437452850765c88ac623
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-08-29 16:12:25 +0200

    add img cw-logo

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Clarusway from '../../media/cw.svg';
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="container">
+            <img src={Clarusway} alt=""/>
+        </div>
+    )
+}
+
+export default Header
