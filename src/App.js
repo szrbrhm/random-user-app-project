1682688c7bd3d9dda6b8462bc53aa39ab8893b1d
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-08-29 17:05:38 +0200

    add card component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Card from './components/Card/Card';
+import axios from 'axios';
 
 function App() {
+  
+ 
+
   return (
     <div className="App">
       <Header/>
+      <Card />
     </div>
   );
 }
