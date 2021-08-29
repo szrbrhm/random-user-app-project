0c864b41988f2da37963d945bdf27695dff0792a
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-08-29 18:01:29 +0200

    add function

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Design from '../../media/design.svg';
+import './footer.css'
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>&#60;ed8en/&#62;</p>
+            <img className="design" src={Design} alt="" />
+            <p>design</p>
+        </div>
+    )
+}
+
+export default Footer;
